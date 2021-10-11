ac01b466101bb9d8f74d89c36fcd2afe92e156ac
thinh.nguyen5 <thinh.nguyen5@tiki.vn>
2021-10-12 00:04:04 +0700

    ! runnable layout
    ! runnable layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { useState } from "react"
+import {
+  RobotOutlined,
+  FireOutlined,
+  DollarOutlined
+} from '@ant-design/icons';
 
 function App() {
+  const { Content, Footer, Sider } = Layout;
+
+  const [state, setState] = useState(false)
+
+  const onCollapse = collapsed => {
+    console.log(collapsed);
+    setState({ collapsed });
+  };
+
+  const { collapsed } = state;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<RobotOutlined />}>
+              Bots
+            </Menu.Item>
+            <Menu.Item key="2" icon={<FireOutlined />}>
+              Trades
+            </Menu.Item>
+            <Menu.Item key="3" icon={<DollarOutlined />}>
+              Balance
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Bot</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
   );
 }
 
