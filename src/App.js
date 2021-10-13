970d115c4a9cc30bc3f096fe362a185ee539db1c
thinh.nguyen5 <thinh.nguyen5@tiki.vn>
2021-10-14 00:02:28 +0700

    ! complete adding router and route
    ! complete adding router and route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import 'antd/dist/antd.dark.css';
 import { useState, useEffect } from "react"
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu } from 'antd';
 import Bots from './components/Bots';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Link } from "react-router-dom"
 
 function App() {
   const { Header, Content } = Layout;
@@ -25,21 +27,41 @@ function App() {
   }, [])
 
   return (
+    <Router>
   <Layout className="layout">
     <Header>
       <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        < Menu.Item key='1'>Bots</Menu.Item>
-        < Menu.Item key='2'>Trades</Menu.Item>
-        < Menu.Item key='3'>Balance</Menu.Item>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        < Menu.Item key='1'>
+          <Link to='/'>Bots </Link> 
+          </Menu.Item>
+        < Menu.Item key='2'>
+        <Link to='/trades'>Closed Trades</Link> 
+          </Menu.Item>
+        < Menu.Item key='3'>
+        <Link to='/orders'>Open Orders</Link> 
+          </Menu.Item>
+        < Menu.Item key='4'>
+        <Link to='/wallet'>Wallet</Link> 
+          </Menu.Item>
       </Menu>
     </Header>
     <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-      </Breadcrumb>
-      <Bots data={botData}/>
+      <Route path='/' exact render={(props) => (
+        <Bots data={botData}/>
+      )} />
+      <Route path='/trades' exact render={(props) => (
+        <p> Trades </p>
+      )} />
+      <Route path='/orders' exact render={(props) => (
+        <p> Orders </p>
+      )} />
+      <Route path='/wallet' exact render={(props) => (
+        <p> Wallet </p>
+      )} />
     </Content>
       </Layout>
+      </Router>
   );
 }
 
