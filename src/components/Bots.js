0ccc4c0ac537b34c6b1a7adfa2518528b120e72f
thinh.nguyen5 <thinh.nguyen5@tiki.vn>
2021-10-14 00:54:04 +0700

    complete runnable button for controlling bots
    complete runnable button for controlling bots

--- src/components/Bots.js
+++ src/components/Bots.js
@@ -3,6 +3,14 @@ import { Row, Col, Card,Typography } from 'antd';
 
 const Bots = ({data}) => {
     const { Text } = Typography;
+
+    const executeCommand = async (bot, action) => {
+        const resp = await fetch(`http://localhost:5000/bots?action=${action}&bot_id=${bot.id}`)
+        const data = await resp.json()
+        console.log(data)
+        return data
+      }
+
     return (
         <>
             <Row>
@@ -15,10 +23,10 @@ const Bots = ({data}) => {
                             <p><Text strong>Total Trades</Text>: {bot.total_trades}</p>
                             <p><Text strong>Winning Trades</Text>: {bot.win_trades}</p>
                             <p><Text strong>Losing Trades</Text>: {bot.lose_trades}</p>
-                            <p><Text strong>Reload Config</Text>: <Button>Reload</Button></p>
-                            <p><Text strong>Start the bot</Text>: <Button>Start</Button></p>
-                            <p><Text strong>Stop the bot</Text>: <Button>Stop</Button></p>
-                            <p><Text strong>Stop buy</Text>: <Button>Stop buy</Button></p>
+                            <p><Text strong>Reload Config</Text>: <Button type='submit' onClick={() => executeCommand(bot, 'reload')}>Reload</Button></p>
+                            <p><Text strong>Start the bot</Text>: <Button type='submit' onClick={() => executeCommand(bot, 'start')}>Start</Button></p>
+                            <p><Text strong>Stop the bot</Text>: <Button type='submit' onClick={() => executeCommand(bot,'stop')}>Stop</Button></p>
+                            <p><Text strong>Stop buy</Text>: <Button type='submit' onClick={() => executeCommand(bot, 'stopbuy')}>Stop buy</Button></p>
                             </Card>
                             </Space>
                     </Col>
