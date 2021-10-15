e99f205d5877f45abf161e4a6de075174adb2576
thinh.nguyen5 <thinh.nguyen5@tiki.vn>
2021-10-15 20:41:51 +0700

    complete mvp open trades list
    complete mvp open trades list

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -1,15 +1,15 @@
-import { Button, Space } from 'antd';
+import { Space } from 'antd';
 import { Row, Col, Card,Typography } from 'antd';
 
 const Trades = ({data}) => {
     const { Text } = Typography;
 
-    const executeCommand = async (trade, action) => {
-        const resp = await fetch(`http://localhost:5000/trades?action=${action}&trade_id=${trade.id}`)
-        const data = await resp.json()
-        console.log(data)
-        return data
-      }
+    // const executeCommand = async (trade, action) => {
+    //     // const resp = await fetch(`http://localhost:5000/trades?action=${action}&trade_id=${trade.id}`)
+    //     // const data = await resp.json()
+    //     console.log(action)
+    //     return data
+    //   }
 
     return (
         <>
@@ -17,13 +17,26 @@ const Trades = ({data}) => {
                 {data.map((trade) => (
                     <Col span={6} xs={24} xl={6}>
                     <Space>
-                        <Card title={trade.exchange + " | " + trade.pair} style={{ width: 300 }}>
-                            <p><Text strong>Id</Text>: {trade.id}</p>
-                            <p><Text strong>Profit (USD)</Text>: {trade.profit_usd}</p>
-                            <p><Text strong>Profit (%)</Text>: {trade.profit_per}</p>
-                            <p><Text strong>Status</Text>: {trade.status}</p>
-                            <p><Text strong>Time</Text>: {trade.time}</p>
-                            <p><Text strong>Delete</Text>: <Button type='submit' onClick={() => executeCommand(trade, 'delete')}>Delete</Button></p>
+                        <Card title={trade.pair + " (" + trade.profit_pct + "%)"} style={{ width: 300 }}>
+                            {/* <p><Text strong>Id</Text>: {trade.trade_id}</p> */}
+                            {/* <p><Text strong>Pair</Text>: {trade.pair}</p> */}
+                            <p><Text strong>Profit (USD)</Text>: {trade.profit_fiat}</p>
+                            <p><Text strong>Profit (%)</Text>: {trade.profit_pct + "%"}</p>
+                            <p><Text strong>Rate (USD)</Text>: {trade.open_rate + " / " + trade.current_rate}</p>
+                            {/* <p><Text strong>Current rate (USD)</Text>: {trade.current_rate}</p> */}
+                            <p><Text strong>Stake (USD)</Text>: {trade.stake_amount}</p>
+                            {/* <p><Text strong>Openning</Text>: {trade.is_open}</p> */}
+                            <p><Text strong>Bot</Text>: {trade.bot_name}</p>
+                            {/* <p><Text strong>Strategy</Text>: {trade.strategy}</p> */}
+                            <p><Text strong>Trade Time</Text>: {trade.trade_elapsed_hr + " hours ago"}</p>
+                            {/* <p>
+                                <Space>
+                                    <Button type='submit' onClick={() => executeCommand(trade, 'forcesell')}>Force Sell</Button>
+                                    <Button type='submit' onClick={() => executeCommand(trade, 'delete')}>Delete</Button>
+                                    <Button type='submit' onClick={() => executeCommand(trade, 'hold_1')}>Hold + 1%</Button>
+                                </Space>
+                            </p> */}
+                            {/* <p><Text strong>Delete</Text>: </p> */}
                             </Card>
                             </Space>
                     </Col>
