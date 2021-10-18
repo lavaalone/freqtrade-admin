c273510fbb20fa4f2c8df497de8bff6405c330d9
thinh.nguyen5 <thinh.nguyen5@tiki.vn>
2021-10-18 23:21:34 +0700

    sub string
    sub string

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -20,9 +20,9 @@ const Trades = ({data}) => {
                         <Card title={trade.pair + " (" + trade.profit_pct + "%)"} style={{ width: 300 }}>
                             {/* <p><Text strong>Id</Text>: {trade.trade_id}</p> */}
                             {/* <p><Text strong>Pair</Text>: {trade.pair}</p> */}
-                            <p><Text strong>Profit (USD)</Text>: {trade.profit_fiat}</p>
+                            <p><Text strong>Profit (USD)</Text>: {trade.profit_fiat.substring(0,6)}</p>
                             {/* <p><Text strong>Profit (%)</Text>: {trade.profit_pct + "%"}</p> */}
-                            <p><Text strong>Rate (USD)</Text>: {trade.open_rate + " / " + trade.current_rate}</p>
+                            <p><Text strong>Rate (USD)</Text>: {trade.open_rate.substring(0,6) + " / " + trade.current_rate.substring(0,6)}</p>
                             {/* <p><Text strong>Current rate (USD)</Text>: {trade.current_rate}</p> */}
                             <p><Text strong>Stake (USD)</Text>: {trade.stake_amount}</p>
                             {/* <p><Text strong>Openning</Text>: {trade.is_open}</p> */}
